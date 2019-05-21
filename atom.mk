3dab87cababcbba21a03773e87144bf3afb65d17
Nicolas Dessart <nicolas.dessart@parrot.com>
2019-05-21 15:07:25 +0200

    [DEV] add libvideo-buffers-generic

--- atom.mk
+++ atom.mk
@@ -10,15 +10,18 @@ LOCAL_EXPAND_CUSTOM_VARIABLES := 1
 
 LOCAL_CUSTOM_MACROS := \
 	pybinding-macro:olympe_deps,$\
-	libpomp:libpdraw:libvideo-buffers:libarsdk:libarsdkctrl,$\
+	libpomp:libpdraw:libvideo-buffers:libvideo-buffers-generic:$\
+	libarsdk:libarsdkctrl,$\
 	LIBPOMP_HEADERS:$\
 	LIBPDRAW_HEADERS:$\
 	LIBVIDEOBUFFERS_HEADERS:$\
+	LIBVIDEOBUFFERSGENERIC_HEADERS:$\
 	LIBARSDK_HEADERS:$\
 	LIBARSDKCTRL_HEADERS,$\
 	$(TARGET_OUT_STAGING)$(shell echo $${TARGET_DEPLOY_ROOT:-/usr})/lib/libpomp$(TARGET_SHARED_LIB_SUFFIX):$\
 	$(TARGET_OUT_STAGING)$(shell echo $${TARGET_DEPLOY_ROOT:-/usr})/lib/libpdraw$(TARGET_SHARED_LIB_SUFFIX):$\
-	$(TARGET_OUT_STAGING)$(shell echo $${TARGET_DEPLOY_ROOT:-/usr})/lib/libvideo-buffers$(TARGET_SHARED_LIB_SUFFIX):$\
+        $(TARGET_OUT_STAGING)$(shell echo $${TARGET_DEPLOY_ROOT:-/usr})/lib/libvideo-buffers$(TARGET_SHARED_LIB_SUFFIX):$\
+	$(TARGET_OUT_STAGING)$(shell echo $${TARGET_DEPLOY_ROOT:-/usr})/lib/libvideo-buffers-generic$(TARGET_SHARED_LIB_SUFFIX):$\
 	$(TARGET_OUT_STAGING)$(shell echo $${TARGET_DEPLOY_ROOT:-/usr})/lib/libarsdk$(TARGET_SHARED_LIB_SUFFIX):$\
 	$(TARGET_OUT_STAGING)$(shell echo $${TARGET_DEPLOY_ROOT:-/usr})/lib/libarsdkctrl$(TARGET_SHARED_LIB_SUFFIX)
 
