f17784e940da2715f439f81ce32c4907a44fe9ed
Nicolas Dessart <nicolas.dessart@parrot.com>
2019-06-13 11:22:38 +0200

    [DEV] add optional libmetadata-thermal

--- atom.mk
+++ atom.mk
@@ -4,25 +4,45 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := olympe-deps
 LOCAL_CATEGORY_PATH := libs
-LOCAL_DESCRIPTION := Python ctypes bindings for olympe dependencies (libpdraw, ....)
+LOCAL_DESCRIPTION := \
+	Python ctypes bindings for olympe dependencies (libpdraw, ....)
 
 LOCAL_EXPAND_CUSTOM_VARIABLES := 1
 
-LOCAL_CUSTOM_MACROS := \
-	pybinding-macro:olympe_deps,$\
+OLYMPE_DEPS_LIBS_NAME := $\
 	libpomp:libpdraw:libvideo-buffers:libvideo-buffers-generic:$\
-	libarsdk:libarsdkctrl,$\
+	libarsdk:libarsdkctrl
+
+OLYMPE_DEPS_HEADERS := $\
 	LIBPOMP_HEADERS:$\
 	LIBPDRAW_HEADERS:$\
 	LIBVIDEOBUFFERS_HEADERS:$\
 	LIBVIDEOBUFFERSGENERIC_HEADERS:$\
 	LIBARSDK_HEADERS:$\
-	LIBARSDKCTRL_HEADERS,$\
-	$(TARGET_OUT_STAGING)$(shell echo $${TARGET_DEPLOY_ROOT:-/usr})/lib/libpomp$(TARGET_SHARED_LIB_SUFFIX):$\
-	$(TARGET_OUT_STAGING)$(shell echo $${TARGET_DEPLOY_ROOT:-/usr})/lib/libpdraw$(TARGET_SHARED_LIB_SUFFIX):$\
-        $(TARGET_OUT_STAGING)$(shell echo $${TARGET_DEPLOY_ROOT:-/usr})/lib/libvideo-buffers$(TARGET_SHARED_LIB_SUFFIX):$\
-	$(TARGET_OUT_STAGING)$(shell echo $${TARGET_DEPLOY_ROOT:-/usr})/lib/libvideo-buffers-generic$(TARGET_SHARED_LIB_SUFFIX):$\
-	$(TARGET_OUT_STAGING)$(shell echo $${TARGET_DEPLOY_ROOT:-/usr})/lib/libarsdk$(TARGET_SHARED_LIB_SUFFIX):$\
-	$(TARGET_OUT_STAGING)$(shell echo $${TARGET_DEPLOY_ROOT:-/usr})/lib/libarsdkctrl$(TARGET_SHARED_LIB_SUFFIX)
+	LIBARSDKCTRL_HEADERS
+
+OLYMPE_DEPS_LIBS_DIR := \
+	$(TARGET_OUT_STAGING)$(shell echo $${TARGET_DEPLOY_ROOT:-/usr})/lib/
+
+OLYMPE_DEPS_LIBS_PATH := $\
+	$(OLYMPE_DEPS_LIBS_DIR)libpomp$(TARGET_SHARED_LIB_SUFFIX):$\
+	$(OLYMPE_DEPS_LIBS_DIR)libpdraw$(TARGET_SHARED_LIB_SUFFIX):$\
+	$(OLYMPE_DEPS_LIBS_DIR)libvideo-buffers$(TARGET_SHARED_LIB_SUFFIX):$\
+	$(OLYMPE_DEPS_LIBS_DIR)libvideo-buffers-generic$(TARGET_SHARED_LIB_SUFFIX):$\
+	$(OLYMPE_DEPS_LIBS_DIR)libarsdk$(TARGET_SHARED_LIB_SUFFIX):$\
+	$(OLYMPE_DEPS_LIBS_DIR)libarsdkctrl$(TARGET_SHARED_LIB_SUFFIX)
+
+ifeq ($(CONFIG_ALCHEMY_BUILD_LIBMETADATATHERMAL),y)
+OLYMPE_DEPS_LIBS_NAME := $(OLYMPE_DEPS_LIBS_NAME):libmetadatathermal
+OLYMPE_DEPS_HEADERS := $(OLYMPE_DEPS_HEADERS):LIBMETADATATHERMAL_HEADERS
+OLYMPE_DEPS_LIBS_PATH := $(OLYMPE_DEPS_LIBS_PATH):$\
+	$(OLYMPE_DEPS_LIBS_DIR)/libmetadatathermal$(TARGET_SHARED_LIB_SUFFIX)
+endif
+
+LOCAL_CUSTOM_MACROS := \
+	pybinding-macro:olympe_deps,$\
+	$(OLYMPE_DEPS_LIBS_NAME),$\
+	$(OLYMPE_DEPS_HEADERS),$\
+	$(OLYMPE_DEPS_LIBS_PATH)
 
 include $(BUILD_CUSTOM)
